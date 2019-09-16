e367c2e40a44c23be6fadf13e191f8014493c9e3
woophee <woophee@outlook.com>
2019-09-16 23:59:25 +0800

    Initial main application

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # woophee-data-transfer
 The data transfer system to receive data and forward data
+一种数据接收和数据转发的系统，支持多数据源和多接收源。
+Source:
+- http
+- kafka
+Sink:
+- Kafka
---
+++ src/main/java/com/woophee/DataTransferApplication.java
@@ -0,0 +1,11 @@
+package com.woophee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataTransferApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataTransferApplication.class, args);
+    }
+}
