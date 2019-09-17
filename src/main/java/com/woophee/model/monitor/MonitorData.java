83f53885e56ff190924466f969ce129c768e2a02
woophee <woophee@outlook.com>
2019-09-18 01:05:51 +0800

    Initial MonitorData model 初始化监控数据的上报模型

--- src/main/java/com/woophee/model/monitor/MonitorData.java
+++ src/main/java/com/woophee/model/monitor/MonitorData.java
@@ -1,4 +1,42 @@
 package com.woophee.model.monitor;
 
+import java.util.Map;
+
 public class MonitorData {
+    private String measurement;
+    private Map<String, String> tags;
+    private Map<String, Object> fields;
+    private long timestamp;
+
+    public String getMeasurement() {
+        return measurement;
+    }
+
+    public void setMeasurement(String measurement) {
+        this.measurement = measurement;
+    }
+
+    public Map<String, String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Map<String, String> tags) {
+        this.tags = tags;
+    }
+
+    public Map<String, Object> getFields() {
+        return fields;
+    }
+
+    public void setFields(Map<String, Object> fields) {
+        this.fields = fields;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
 }
